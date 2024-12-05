39c683e2e919ba7cdccbc01fed9f7a4b89bfbfe1
Dican <jdescalante1311@gmail.com>
2024-12-05 15:47:35 -0600

    solve excercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR"; // Manejo del caso de números negativos
+    let repeatedString = "";
+    for (let i = 0; i < num; i++) {
+        repeatedString += str; // Agrega la cadena al resultado en cada iteración
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
