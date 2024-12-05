3eae89929100d2ff7a494280f141b2ae561e4058
Dican <jdescalante1311@gmail.com>
2024-12-05 16:06:26 -0600

    Solve problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let reversedString = "";
+for(let i= str.length; i>=0 ; i--)
+    {
+        reversedString += str.charAt(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
