f36c83eefd248ab6704c330dd1487d115ce1534a
Dican <jdescalante1311@gmail.com>
2024-12-06 22:23:00 -0600

    solve exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
-const leapYears = function() {
+const leapYears = num => (num % 4 === 0 && (num % 100 !== 0 || num % 400 === 0));
 
-};
+
 
 // Do not edit below this line
 module.exports = leapYears;
