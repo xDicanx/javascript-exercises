04308539bb3c14ae86a51278d422baaff0eb57ed
Dican <jdescalante1311@gmail.com>
2024-12-06 21:31:13 -0600

    complete excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstnumber, lastnumber) {
+    if(firstnumber>lastnumber)
+        {
+            let biggestnumber = firstnumber;
+            firstnumber = lastnumber;
+            lastnumber = biggestnumber;
+        }
+    if(firstnumber < 0 || lastnumber < 0 || !Number.isInteger(firstnumber) || !Number.isInteger(lastnumber))
+        {
+            return "ERROR";
+        }
+        let totalsum = 0;
+        for(let i = firstnumber; i <= lastnumber; i++)
+            {
+                totalsum += i;
+            }
+            return totalsum;
 
 };
 
