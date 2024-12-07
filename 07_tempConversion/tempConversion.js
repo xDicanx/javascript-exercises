41c3b2f45c4e6614da946dd3a6923dbef8c0775d
Dican <jdescalante1311@gmail.com>
2024-12-07 11:20:46 -0600

    add as reference an optimized code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -8,6 +8,20 @@ const convertToFahrenheit = function(celsius) {
   return parseFloat(fahrenheit.toFixed(1));
 };
 
+//Optimized by chatgpt
+/*
+
+const roundToOneDecimal = (num) => parseFloat(num.toFixed(1));
+
+
+const convertToCelsius = (fahrenheit) => 
+    roundToOneDecimal((fahrenheit - 32) * 5 / 9);
+
+
+const convertToFahrenheit = (celsius) => 
+    roundToOneDecimal((celsius * 9 / 5) + 32);
+*/
+
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
