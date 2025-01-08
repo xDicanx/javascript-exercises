5bde28c89cc2ebc12f683e37c0a54b3517fba399
Dican <jdescalante1311@gmail.com>
2025-01-08 14:16:30 -0600

    Solve calculator problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return Array.isArray(array) 
+    ? array.reduce((totalSum, actualNumber) => totalSum + actualNumber, 0)
+    : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return Array.isArray(array) 
+  ? array.reduce((totalSum, actualNumber) => totalSum * actualNumber, 1)
+  : 0;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
